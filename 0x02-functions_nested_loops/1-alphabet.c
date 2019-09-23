f62ef2a5715a1907abbdde67f53f9f434532db6b
Gtmigue <migue.gonzalez-@hotmail.com>
2019-09-23 23:15:52 +0000

    call the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int mig;
+for (mig = 'a'; mig <= 'z'; mig++)
+{
+_putchar(mig);
+}
+_putchar('\n');
+return (0);
+}
