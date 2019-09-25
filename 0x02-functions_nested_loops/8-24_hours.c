9188a91f8481dda576579a7b89891fdfa5ba217a
Gtmigue <migue.gonzalez-@hotmail.com>
2019-09-25 03:29:04 +0000

    h

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,17 +5,19 @@
  */
 void jack_bauer(void)
 {
-int a, b;
-for (a = 0; a <= 23 ; a++)
+int h, hh;
+for (h = 0; h <= 23 ; h++)
 {
-for (b = 0; b <= 59; b++)
+for (hh = 0; hh <= 59; hh++)
 {
-_putchar(a / 10 + '0');
-_putchar(a % 10 + '0');
+_putchar(h / 10 + '0');
+_putchar(h % 10 + '0');
 _putchar(':');
-_putchar(b / 10 + '0');
-_putchar(b % 10 + '0');
+_putchar(hh / 10 + '0');
+_putchar(hh % 10 + '0');
 _putchar('\n');
 }
 }
 return;
+}
+
